c531d4e076c782ecd11c5082bea7af3e90c6388b
Krzysztof Urbaniak <urban@fail.pl>
2016-09-01 15:07:17 +0200

    using pbr

--- setup.py
+++ setup.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python
-"""
-CEE logging module formatter.
-"""
 from setuptools import setup
 
 
 setup(
-    name='cee-formatter',
-    version='0.3',
-    url='https://github.com/urbaniak/cee-formatter',
-    license='BSD',
-    author='Krzysztof Urbaniak',
-    author_email='urban@fail.pl',
-    description='CEE logging module formatter.',
-    long_description=__doc__,
-    py_modules=['cee_formatter'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
