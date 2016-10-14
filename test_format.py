2f3f821ff36684a0603804c918995a4cb050b56d
Jakub Stasiak <jakub.stasiak@smarkets.com>
2016-10-14 19:05:48 +0200

    Format time like isoformat() would
    In many applications millisecond precision is not enough to understand
    what's going on, let's stick to microsecond-precision like with
    isoformat(). Additionally the separators are changed to match those of
    isoformat() (which is used to format other timestamps here).

--- test_format.py
+++ test_format.py
@@ -2,6 +2,8 @@ import datetime
 import json
 import logging
 
+import iso8601
+
 from cee_formatter import CEEFormatter
 
 try:
@@ -36,3 +38,11 @@ def test_datetime_format():
     json_dict = json.loads(json_value)
 
     assert json_dict['d'] == date.isoformat()
+
+    # We want to make sure that the time is in full ISO8601 format so
+    # we don't lose too much precision (it'll be microseconds).
+    # This assertion can generate a false positive when timestamp has
+    # a zero microsecond part but I think we can live with it.
+    serialized_timestamp = json_dict['time']
+    timestamp = iso8601.parse_date(serialized_timestamp).replace(tzinfo=None)
+    assert serialized_timestamp == timestamp.isoformat()
